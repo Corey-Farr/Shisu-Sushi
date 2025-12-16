453193d989bf11171a94cecd6f6bc6b5186d2416
Corey-Farr <corey.p.farr@gmail.com>
2025-12-16 10:25:50 -0700

    Navbar bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ export function Navbar() {
           {navItems.map((item) => (
             <Link
               key={item.href}
-              href={item.href}
+              href={item.href as any}
               className="hover:text-white"
             >
               {item.label}
